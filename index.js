9424460fae2ee0e13227d1bdc121da9f9b07fd7e
Rob Ireton <rob@ireton.org>
2023-02-03 16:23:23 -0500

    express scaffolding

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-import ingest from './lib/ingest.js'
+// import ingest from './lib/ingest.js'
+import Server from './lib/server.js'
 
-ingest('./user_1002061_1675089365.zip')
+const server = new Server({
+  host: '::1',
+  port: 3569
+})
+console.log(`listening on ${server.address}:${server.port}`)
+
+// ingest('./user_1002061_1675089365.zip')
