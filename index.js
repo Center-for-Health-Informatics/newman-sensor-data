62ac8453ab7cd241ca6f36a7cf5316cac00f485b
Rob Ireton <rob@ireton.org>
2023-02-06 15:59:45 -0500

    summary page

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const server = new Server({
   host: '::1',
   port: 3569
 })
-console.log(`listening on ${server.address}:${server.port}`)
+server.on('listening', (address, port) => console.log(`listening on ${address}:${port}`))
 
 // ingest('./user_1002061_1675089365.zip')
